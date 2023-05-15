29d6bd1a37b783617d750a7c0961545d2edbe57e
jkroger <jkroger@student.42wolfsburg.de>
2023-05-15 10:08:22 +0200

    account added

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkroger <jkroger@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 19:21:14 by jkroger           #+#    #+#             */
+/*   Updated: 2023/05/14 19:21:14 by jkroger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+/* : _accountIndex(Account::_nbAccounts),
+		_amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)  */
+
+Account::Account	(int initial_deposit) 
+{
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+	_nbAccounts--;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit;
+	_totalNbDeposits++;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	this->_amount += deposit;
+	std::cout << ";amount:" << this->_amount << ";nb_deposits" << this->_nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	if (this->_amount - withdrawal < 0)
+	{
+		std::cout << "Not enough money for withdrawal!" << std::endl;
+		return (false);
+	}
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:" << withdrawal;
+	_totalNbWithdrawals++;
+	this->_nbWithdrawals++;
+	_totalAmount -= withdrawal;
+	this->_amount -= withdrawal;
+	std::cout << ";amount:" << this->_amount << ";nb_withdrawals" << this->_nbWithdrawals << std::endl;
+	return (true);
+}
+
+int	Account::checkAmount(void) const{
+	return (this->_amount);
+}
+
+void	Account::displayStatus(void) const{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits;
+	std::cout << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:";
+	std::cout << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::_displayTimestamp(void)
+{
+	time_t	t = time(NULL);
+	tm	*local_time = localtime(&t);
+	std::cout << "[" << local_time->tm_year + 1900;
+	std::cout << std::setw(2) << std::setfill('0') << local_time->tm_mon;
+	std::cout << std::setw(2) << std::setfill('0') << local_time->tm_mday;
+	std::cout << "_" << std::setw(2) << std::setfill('0') << local_time->tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << local_time->tm_min;
+	std::cout << std::setw(2) << std::setfill('0') << local_time->tm_sec << "] ";
+}
