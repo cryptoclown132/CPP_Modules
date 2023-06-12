98fa600a0232249669a08dc4b3c41e0d3b091cb0
jkroger <jkroger@student.42wolfsburg.de>
2023-06-12 09:50:35 +0200

    new test mains for cpp07

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -12,11 +12,43 @@
 
 #include "iter.hpp"
 
-int	main()
-{
-	std::string	arr[5] = {"ab", "cd", "ef", "gh", "ij"};
-	iter(arr, 5, print);
+// int	main()
+// {
+// 	std::string	arr[5] = {"ab", "cd", "ef", "gh", "ij"};
+// 	iter(arr, 5, print);
 
-	int	brr[6] = {12, 3432, -22, 23, 45, 120};
-	iter(brr, 6, print);
+// 	int	brr[6] = {12, 3432, -22, 23, 45, 120};
+// 	iter(brr, 6, print);
+// }
+
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
